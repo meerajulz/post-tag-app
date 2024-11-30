e09d1f71a02679d0eb0c02b054b8d8e5deb8ef40
Juliana Leon <juleon80@gmail.com>
2024-11-30 16:32:20 +0100

    wip: add option to select tag from list

--- src/components/input-message/input-message.component.tsx
+++ src/components/input-message/input-message.component.tsx
@@ -5,10 +5,39 @@ import { MessageContainer, CustomTextarea } from './input-message.styles';
 
 const InputMessage: React.FC = () => {
   const [inputText, setInputText] = useState<string>('');
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+  const [currentSuggestions, setCurrentSuggestions] = useState<string[]>([]);
+
+  useEffect(() => {
+    // Only filter out tags that start with '#' from input and adjust logic to match suggestions without '#'
+    const hashtagParts = inputText
+      .split(/(\s+)/)
+      .filter((t) => t.trim().length > 0 && t.trim().startsWith('#'));
+    const lastTag = hashtagParts.pop() || '';
+    const tagQuery = lastTag.slice(1); // Remove '#' for matching
+    if (tagQuery) {
+      const matchedSuggestions = tagList.filter(
+        (s) => s.startsWith(tagQuery) && `#${s}` !== lastTag
+      );
+      setCurrentSuggestions(matchedSuggestions);
+      setSelectedIndex(0);
+    } else {
+      setCurrentSuggestions([]);
+    }
+  }, [inputText]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInputText(e.target.value);
-    console.log(inputText);
+  };
+
+  const addTagToInput = (tag: string) => {
+    // Append '#' when adding a tag from suggestions
+    const parts = inputText.split(/(\s+)/);
+    const withoutLastTag = parts.slice(0, -1).join('');
+    const updatedText = `${withoutLastTag}#${tag} `;
+    setInputText(updatedText);
+    inputRef.current?.focus();
   };
 
   return (
@@ -17,8 +46,28 @@ const InputMessage: React.FC = () => {
       <CustomTextarea
         onChange={handleInputChange}
         value={inputText}
+        ref={inputRef}
         placeholder="What’s happening?"
       />
+
+      {/* tag */}
+      {currentSuggestions.length > 0 && (
+        <ul style={{ listStyleType: 'none', padding: 0 }}>
+          {currentSuggestions.map((suggestion, index) => (
+            <li
+              key={index}
+              style={{
+                cursor: 'pointer',
+                backgroundColor:
+                  selectedIndex === index ? '#eee' : 'transparent',
+              }}
+              onClick={() => addTagToInput(suggestion)}
+            >
+              #{suggestion} {/* Display suggestion with '#' prefix */}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
