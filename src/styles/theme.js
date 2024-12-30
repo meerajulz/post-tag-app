0a0862c254540454c8972db6b5fbe562e8abe8c3
Juliana Leon <juleon80@gmail.com>
2024-12-30 13:13:23 +0100

    theme

---
+++ src/styles/theme.js
@@ -0,0 +1,19 @@
+const theme = {
+    colors: {
+        primary: '#1da1f2',
+        text: '#ccc',
+        black: '#070707',
+        selection: '#eee',
+        blue: '#1da1f2',
+        green: '#4EB840',
+        border: '#ccc',
+        inherit: 'inherit',
+    },
+    fontSize: '20px',
+    lineHeight: '20px',
+    fontWeight: '100',
+    fontFamily: 'Arial, sans-serif',
+};
+
+export default theme;
+export { theme };
